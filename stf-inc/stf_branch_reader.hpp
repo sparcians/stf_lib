071fb6b3cf3df0303ba03fac7b98f5788a4e5998
Brett Dutro <brett.dutro@sifive.com>
2023-06-01 11:50:47 -0700

    Revert "STFBranchReader: Always initialize REG_0 in src/dest operand state maps"
    This reverts commit 07f808cdb4bd93394c4655019cd7063fd77e7b97.
    
    The original patch fixed a bug (checking RS1 twice instead of checking
    RS1 and RS2), but broke detection of JAL/JR instructions since it added
    superfluous X0 src/dest operands. This reverts the patch and fixes the
    original RS1/RS2 bug.

--- stf-inc/stf_branch_reader.hpp
+++ stf-inc/stf_branch_reader.hpp
@@ -85,10 +85,7 @@ namespace stf {
             __attribute__((hot, always_inline))
             inline void resetOperandMaps_() {
                 src_operands_.clear();
-                src_operands_.addOperand(Registers::STF_REG::STF_REG_X0, 0);
-
                 dest_operands_.clear();
-                dest_operands_.addOperand(Registers::STF_REG::STF_REG_X0, 0);
             }
 
             template<typename InstRecordType>
@@ -112,6 +109,17 @@ namespace stf {
                     delegates::STFBranchDelegate::reset_(branch);
                     resetOperandMaps_();
                     return false;
+                }
+
+                // X0 records may not exist in the trace, so fix up the operand maps if necessary
+                if(STF_EXPECT_FALSE(((branch.getRS1() == Registers::STF_REG::STF_REG_X0) ||
+                                     (branch.getRS2() == Registers::STF_REG::STF_REG_X0)) &&
+                                    !src_operands_.hasOperand(Registers::STF_REG::STF_REG_X0))) {
+                    src_operands_.addOperand(Registers::STF_REG::STF_REG_X0, 0);
+                }
+                if(STF_EXPECT_FALSE((branch.getRD() == Registers::STF_REG::STF_REG_X0) &&
+                                    !dest_operands_.hasOperand(Registers::STF_REG::STF_REG_X0))) {
+                    dest_operands_.addOperand(Registers::STF_REG::STF_REG_X0, 0);
                 }
 
                 delegates::STFBranchDelegate::setOperandValues_(branch, src_operands_, dest_operands_);
